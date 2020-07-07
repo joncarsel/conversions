2d0da40fa071de52a935410a379657c878ba68d7
joncarsel <joncarsel@gmail.com>
2020-07-07 17:26:35 +0300

    needs testing...moved functions in to modules

---
+++ App.js
@@ -0,0 +1,4 @@
+import {binaryToDecimalOf} from "./js/modules.js";
+document.write("heys");
+document.write(binaryToDecimalOf(1010));
+document.write("heys");
--- js/modules.js
+++ js/modules.js
@@ -1,6 +1,32 @@
 export default function sayHello(name) {
     return ('hello' + name);
 }
-export function sayHelloAgain(name) {
-    return ('hello again from ' + name);
+
+export function binaryToDecimalOf(bin) {
+  var decimal = 0;
+  for (let index = bin.length - 1; index >= 0; index--) {
+    decimal += parseInt(bin[index]) * Math.pow(2, bin.length - 1 - index);
+  }
+  return decimal;
 }
+
+export function decimalToBinaryOf(decimal) {
+    let binary = "";
+    let powersOfTwo = [];
+    let highestPowerOfTwo = 1;
+
+    while (highestPowerOfTwo <= decimal) {
+        powersOfTwo.unshift(highestPowerOfTwo);
+        highestPowerOfTwo *= 2;
+    }
+    for (var value of powersOfTwo) {
+        if (decimal >= value) {
+            binary += "1";
+            decimal -= value;
+        } else {
+            binary += "0";
+        }
+    }
+    return binary;
+}
+
