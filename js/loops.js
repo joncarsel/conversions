2f14ca7f706e097cdd6b0acc66e67fc3af035848
joncarsel <joncarsel@gmail.com>
2020-07-07 08:47:07 +0300

    first steps

---
+++ js/loops.js
@@ -0,0 +1,11 @@
+var x = 10;
+let name = 'john';
+document.write(name);
+console.log(name.length);
+for (var i = 0; i < x; i++) {
+    document.write(`hello ${x} `);
+}
+arr = new Array(1, 1, 1, 1);
+for (var i = 0; i < x; i++) {
+    document.writeln(arr);
+}
---
+++ js/modules.js
@@ -0,0 +1,6 @@
+export default function sayHello(name) {
+    return ('hello' + name);
+}
+export function sayHelloAgain(name) {
+    return ('hello again from ' + name);
+}
